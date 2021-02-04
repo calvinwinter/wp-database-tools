13ef5acb4528b2f2e16c13275b181efb9732fb53
Calvin Winter <calvin.wlinter@gmail.com>
2021-02-04 14:50:55 -0500

    disable usage tracking opt-in notice

--- template/pro/notice-enable-usage-tracking.php
+++ template/pro/notice-enable-usage-tracking.php
@@ -1,4 +1,4 @@
-<div class="below-title wpmdb-usage-tracking-notice warning inline-message">
+<!--<div class="below-title wpmdb-usage-tracking-notice warning inline-message">
 	<strong>Help Us Improve WP Migrate DB Pro</strong>
 	<p>
 		<?php printf( __( 'We are constantly working to make WP Migrate DB Pro the best that it can be, but that\'s hard to do if we don\'t know how you\'re using it. Please enable data sharing so that we will receive <a href="%s" target="_blank">some information</a> about each migration that you run. We will use this data to improve WP Migrate DB Pro and provide you with better support. This setting can be changed at any time from the Settings tab.', 'wp-migrate-db' ), 'https://deliciousbrains.com/wp-migrate-db-pro/doc/usage-tracking/' ); ?>
@@ -7,4 +7,4 @@
 		<a href="" class="wpmdb-opt-usage-tracking opt-yes">Yes, I'd Like to Help</a> |
 		<a href="" class="wpmdb-opt-usage-tracking opt-no">No</a>
 	</p>
-</div>
+</div>-->
